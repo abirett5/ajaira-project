fa73d809ae49da7b18462a5214bf1448b582d964
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2025-01-21 13:36:29 +0600

    Updated

--- js/script.js
+++ js/script.js
@@ -1,31 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const numberElement = document.getElementById('number');
-  const targetNumber = 500; // The number you want to animate to
-  const duration = 1500; // Duration of the animation in milliseconds
-
-  const animateNumber = () => {
-      let startTimestamp = null;
-      const startNumber = 0;
-
-      const step = (timestamp) => {
-          if (!startTimestamp) startTimestamp = timestamp;
-          const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-          numberElement.textContent = Math.floor(progress * targetNumber);
-          if (progress < 1) {
-              window.requestAnimationFrame(step);
-          }
-      };
-
-      window.requestAnimationFrame(step);
-  };
-
-  const handleScroll = () => {
-      const rect = numberElement.getBoundingClientRect();
-      if (rect.top < window.innerHeight && rect.bottom >= 0) {
+    const numberElement = document.getElementById('number');
+    const numberTwoElement = document.getElementById('number-two');
+    const targetNumber = 500; // The number you want to animate to
+    const targetNumberTwo = 1300; // The number you want to animate to
+    const duration = 1500; // Duration of the animation in milliseconds
+  
+    const animateNumber = () => {
+        let startTimestamp = null;
+        const startNumber = 0;
+  
+        const step = (timestamp) => {
+            if (!startTimestamp) startTimestamp = timestamp;
+            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+            numberElement.textContent = Math.floor(progress * targetNumber);
+            numberTwoElement.textContent = Math.floor(progress * targetNumberTwo);
+            if (progress < 1) {
+                window.requestAnimationFrame(step);
+            }
+        };
+  
+        window.requestAnimationFrame(step);
+    };
+  
+    const handleScroll = () => {
+        const rect = numberElement.getBoundingClientRect();
+        const rectTwo = numberTwoElement.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom >= 0) {
+            animateNumber();
+            window.removeEventListener('scroll', handleScroll);
+        }
+  
+        if (rectTwo.top < window.innerHeight && rectTwo.bottom >= 0) {
           animateNumber();
           window.removeEventListener('scroll', handleScroll);
       }
-  };
-
-  window.addEventListener('scroll', handleScroll);
-});
+    };
+  
+    window.addEventListener('scroll', handleScroll);
+  });  
